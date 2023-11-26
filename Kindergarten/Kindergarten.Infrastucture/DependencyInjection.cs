962ad22cd80cfbf6d2bbf851933eb8714c8c5e27
shokh_hojiyev <hojiyevshohijahon@gmail.com>
2023-11-27 02:48:44 +0500

    Token Added

--- Kindergarten/Kindergarten.Infrastucture/DependencyInjection.cs
+++ Kindergarten/Kindergarten.Infrastucture/DependencyInjection.cs
@@ -1,7 +1,14 @@
-﻿using Kindergarten.Infrastucture.Persistance;
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Domain.Entities;
+using Kindergarten.Infrastucture.Abstractions;
+using Kindergarten.Infrastucture.Persistance;
+using Kindergarten.Infrastucture.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Kindergarten.Infrastucture
 {
@@ -13,6 +20,44 @@ namespace Kindergarten.Infrastucture
             {
                 options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddScoped<ITokenService, JwtService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IHashService, HashServise>();
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuer = true,
+                      ValidateIssuerSigningKey = true,
+                      ValidateLifetime = true,
+                      ValidateAudience = true,
+                      ValidIssuer = configuration["JWT:ValidIssuer"],
+                      ValidAudience = configuration["JWT:validAudiense"],
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]!))
+                  };
+              });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminActions", polisy =>
+                {
+                    polisy.RequireClaim("Roles", Domain.Enums.Roles.Admin.ToString());
+                });
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("TeacherActions", polisy =>
+                {
+                    polisy.RequireClaim("Roles", Domain.Enums.Roles.Teacher.ToString());
+                });
+            });
+
 
             return services;
         }
